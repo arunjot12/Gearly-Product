7f0f2091d9c4e57e55e74f7dd872c39bc51e2633
Arunjot Singh <arunjot@antiersolutions.com>
2026-09-22 15:44:39 +0530

    Updated the cors again

--- src/cors.rs
+++ src/cors.rs
@@ -1,9 +1,13 @@
-use tower_http::cors::{Any, CorsLayer};
 use axum::http::HeaderValue;
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn cors_allow() -> CorsLayer {
     CorsLayer::new()
-        .allow_origin(HeaderValue::from_static("https://gearly-frontend.vercel.app/"))
+        .allow_origin([
+            HeaderValue::from_static("https://gearly-frontend.vercel.app"),
+            HeaderValue::from_static("https://gearly-frontend.vercel.app/"),
+            HeaderValue::from_static("http://localhost:5173"),
+        ])
         .allow_methods(Any)
         .allow_headers(Any)
 }
