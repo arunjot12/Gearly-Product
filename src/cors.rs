2e947dc3e498726538cebaa4a5c14eac643904f9
Arunjot Singh <arunjot@antiersolutions.com>
2026-09-22 14:59:42 +0530

    Updated the cors

--- src/cors.rs
+++ src/cors.rs
@@ -3,7 +3,7 @@ use axum::http::HeaderValue;
 
 pub fn cors_allow() -> CorsLayer {
     CorsLayer::new()
-        .allow_origin(HeaderValue::from_static("http://localhost:5173"))
+        .allow_origin(HeaderValue::from_static("https://gearly-frontend.vercel.app/"))
         .allow_methods(Any)
         .allow_headers(Any)
 }
